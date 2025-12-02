3c707f64b15bcf741c119ce67937b3790bd883c1
Honzqq_ <honzqq_q@pm.me>
2025-12-02 15:57:08 +0100

    updated

--- thermometer.py
+++ thermometer.py
@@ -1,17 +1,18 @@
 def celsius_to_fahrenheit(c):
     fahrenheit = (c * 9/5) + 32
-    return fahrenheit
+    return round (fahrenheit, 1)
 
 def fahrenheit_to_celsius(f):
     celsius = (f - 32) / 1.8
-    return celsius
+    return round (celsius, 1)
 
 selection = input('Select "C" or "F": ')
-thermometer = float(input("Write down your numbers: "))
 
 if selection == "C":
+    thermometer = float(input("Write down your numbers: "))
     print(f"Transfer from C° to F° is: {celsius_to_fahrenheit(thermometer)}°")
 elif selection == "F":
+    thermometer = float(input("Write down your numbers: "))
     print(f"Transfer from F° to C° is: {fahrenheit_to_celsius(thermometer)}°")
 else:
     print("You used the wrong letters!")
